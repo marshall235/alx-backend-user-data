8ee1bd26c035fbf625ada9b0269729d767ca09a3
marshall235 <fredie1738@gmail.com>
2024-11-10 11:03:32 +0300

    Update the class to accept a list of strings fields constructor argument.
    Implement the format method to filter values in incoming log records using filter_datum. Values for fields in fields should be filtered.
    
    DO NOT extrapolate FORMAT manually. The format method should be less than 5 lines long.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,3 +31,32 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """
+        Constructor method for RedactingFormatter class
+        Args:
+            fields: list of fields to redact in log messages
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the specified log methods as a text
+
+        Filters incoming log records using filter_datum
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+
+        return super(RedactingFormatter, self).format(record)
