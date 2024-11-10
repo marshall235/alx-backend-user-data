275fe30fa5b0a75a6047cbf41c64bbd8a4764e82
marshall235 <fredie1738@gmail.com>
2024-11-10 10:01:26 +0300

    a function called filter_datum that returns the log message obfuscated:
    Arguments:
    fields: a list of strings representing all fields to obfuscate
    redaction: a string representing by what the field will be obfuscated
    message: a string representing the log line
    separator: a string representing by which character is separating all fields in the log line (message)
    The function should use a regex to replace occurrences of certain field values.
    filter_datum should be less than 5 lines long and use re.sub to perform the substitution with a single regex.

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+
+from typing import List
+import logging
+import re
+
+
+# PII to be reducted
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Replaces sensitive information in a message
+    with redacted value based on the list of Fields
+    to redact
+     Args:
+        fields: list of fields to redact
+        redaction: the value to use for redaction
+        message: the string message to filter
+        separator: the separator to use between fields
+    Returns:
+        The filtered string message with redacted values
+    """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
