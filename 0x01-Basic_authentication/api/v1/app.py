db9caddaaf599920641475165c69576abc45c854
marshall235 <fredie1738@gmail.com>
2024-11-13 02:21:46 +0300

    Forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -40,7 +40,7 @@ def before_request():
             if auth.authorization_header(request) is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
-                abort(403, description="Forbiddedn")
+                abort(403, description="Forbidden")
 
 
 @app.errorhandler(404)
